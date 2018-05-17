21047071d903606d5f5dace736088ddd15cfc2ee
Martin Handley <psxmah@nottingham.ac.uk>
2018-05-17 20:03:59 +0100

    latest

--- AutoBench/Types.hs
+++ AutoBench/Types.hs
@@ -35,10 +35,12 @@ module AutoBench.Types
 
   ) where
 
-import           Criterion.Types    (OutlierEffect)
-import qualified Criterion.Types as Criterion
-import qualified Criterion.Main  as Criterion
-import           Data.Default       (Default(..))
+import           Control.Exception.Base (Exception)
+import           Criterion.Types        (OutlierEffect)
+import qualified Criterion.Types        as Criterion
+import qualified Criterion.Main         as Criterion
+import           Data.Default           (Default(..))
+
 
 -- AutoBench
 import AutoBench.AbstractSyntax (Id)
@@ -51,7 +53,7 @@ import AutoBench.AbstractSyntax (Id)
 
 -- | Test suites are AutoBench's principle user input datatype, and are used to 
 -- structure performance tests into logical units that can be checked, verified, 
--- and ultimately executed independently. 
+-- and executed independently. 
 --
 -- An advantage of this approach is that users can group multiple test 
 -- suites in the same file according to some testing context, whether it be 
@@ -62,7 +64,7 @@ import AutoBench.AbstractSyntax (Id)
 -- executed nonetheless.
 --
 -- Test suites contain a significant number of user options and settings. As 
--- such, the system provides sensible defaults,
+-- such, the system provides the following defaults,
 --
 -- @ TestSuite
 --     { _progs    = []                                     -- All programs in the test file will be considered for test purposes.
@@ -78,7 +80,7 @@ import AutoBench.AbstractSyntax (Id)
 -- that users can override.
 --
 -- Important note: the most basic check that the system performs on every test
--- suite is to ensure that each of its record fields are initialised. Please
+-- suite is to ensure that each of its record fields are initialised: please
 -- ensure test suites are fully defined.
 data TestSuite = 
   TestSuite
@@ -102,83 +104,116 @@ instance Default TestSuite where
           , _nf       = True
           , _ghcFlags = []                         -- /No/ optimisation, i.e., -O0. 
           }
-
--- ** Configuration
-
-data ABCfg = ABCfg{}
   
--- ** Data options 
+-- ** Test data options
 
-
-
-
--- | For a unary test program @p :: a -> b@, each manual input is of type 
--- @(Int, IO a)@. 
--- In addition, each input /must/ incorporate a sensible notion of size, which 
--- is given as the first element of each @(Int, IO a)@ tuple. For example, in 
--- the case of a unary test program with @[Char]@ input type, we may provide 
--- manual test data @tDat@ as such:
+-- | @type UnaryTestData a = [(Int, IO a)]@.
 --
--- @ tDat :: UnaryTestData [Char]
--- dat  = 
+-- Due to certain benchmarking requirements, test data must be specified in 
+-- the IO monad. In addition, the system cannot determine the size of 
+-- user-specified test data automatically. As such, for a test program 
+-- @p :: a -> b@ , user-specified test data is of type @[(Int, IO a)]@, where 
+-- the first element of each tuple is the size of the test input, and the second 
+-- element is the input itself. 
+--
+-- Concrete example: test program @p :: [Int] -> [Int]@, user-specified test 
+-- data @tDat@.
+--
+-- @ tDat :: UnaryTestData [Int]
+-- tDat = 
 --   [ ( 5
---     , return [/'a/', /'b/', /'c/', /'d/', /'e/'] )
+--     , return [1,2,3,4,5] 
+--     )
 --   , ( 10 
---     , return [/'a/', /'b/', /'c/', /'d/', /'e/', /'f/', /'g/', /'h/', /'i/', /'j/'] )
+--     , return [1,2,3,4,5,6,7,8,9,10] 
+--     )
 --   ... ]@
 -- 
--- Here the size of a list is determined by its number of elements (@5@ and @10@ 
--- respectively).
+-- Here the size of each @[Int]@ is determined by its number of elements 
+-- (@5@ and @10@, respectively).
 --
--- N.B. The @IO@ type is required by 'Criterion' for benchmarking purposes (see 
--- 'Criterion.Main.env'). Fittingly, it also allows users to produce test data 
--- via some impure action if appropriate.
---
--- 'TestSuite's require a minimum number of /distinctly sized/ test inputs: see 
--- 'minInputs'.
+-- Note: test suites require a minimum number of /distinctly sized/ test 
+-- inputs: see 'minInputs'.
 -- 
--- __Important__: Each test datatype must incorporate a /sensible/ notion of 
--- size. For manual test data, the size of each input is specified 
--- manually, and therefore, this value must be carefully considered.
 -- __**Incorrectly sized test data will lead to erroneous performance results**__.
 type UnaryTestData a = [(Int, IO a)]  
 
--- | /Sized/ test data can either be specified manually by users or generated 
--- automatically by the system. 
+-- | @type BinaryTestData a b = [(Int, Int, IO a, IO b)]@
 --
--- If given manually, then the 'Manual' data option simply tells the system 
--- which test data in the user input file to use. See 'UnaryTestData' and 
--- 'BinaryTestData' for details regarding the /types/ of manual test data. 
+-- See 'UnaryTestData' for a discussion on user-specified test data and a
+-- relevant example for unary test programs. This example generalises to
+-- user-specified test data for binary test programs in the obvious way:
+-- 
+-- @tDat :: BinaryTestData [Char] [Int]
+-- tDat = 
+--   [ ( 5
+--     , 4
+--     , return [/'a/', /'b/', /'c/', /'d/', /'e/']
+--     , return [0, 1, 2, 3] )
+--   , ( 10 
+--     , 9
+--     , return [/'a/', /'b/', /'c/', /'d/', /'e/', /'f/', /'g/', /'h/', /'i/', /'j/'] )
+--     , return [0, 1, 2, 3, 4, 5, 6, 7, 8]
+--   ... ]@
 --
--- In the case where inputs are to be generated by the system, users must 
--- specify the /size/ of each input to be generated. The input types of test 
--- programs (which /must/ be the same) determine the /type/ of the generated 
--- data. For example, for a test program @p :: [Int] -> [Int]@, the @Gen@ option 
--- would instruct the system to generate lists of integers of different sizes. 
--- In this instance, the size of each list is determined by @Gen l s u@, which
--- specifies a size /range/ by a lower bound @l@, an upper bound @u@, and a step 
--- @s@. This is converted to the Haskell range @[l, (l + s) .. u]@ and a 
--- list of integers is generated for each size in this range. For example, 
--- @Gen 5 5 100@ corresponds to the range @[5, 10 .. 100]@. 
+-- 'TestSuite's require a minimum number of /distinctly sized/ test datums: see 
+-- 'minInputs'. In the case of 'BinaryTestData', /pairs/ of sizes must be 
+-- distinct. For example, @(5, 4)@ and @(10, 9)@ above are two distinct pairs of 
+-- sizes.
+--
+-- __**Incorrectly sized test data will lead to invalid performance results**__.
+type BinaryTestData a b = [(Int, Int, IO a, IO b)]
+
+-- | Test data can either be specified by users or generated automatically by 
+-- the system. Note: the default setting for 'DataOpts' is @Gen 5 5 100@.
+--
+-- If users choose to specify their own inputs, then the 'Manual' data option 
+-- simply tells the system the name of the test data in the user input file.
+-- For example: 
+--
+-- @ module UserInput where 
+--
+-- tProg :: [Int] -> [Int]
+-- tProg  = ...
+--
+-- tDat :: UnaryTestData [Int]
+-- tDat  = ...
+--
+-- tOpts :: TestOpts 
+-- tOpts  = def { _progs = ["tProg"], _dataOpts = Manual "tDat" }
+-- @
+--
+-- See 'UnaryTestData' and 'BinaryTestData' for details regarding the /types/ 
+-- of user-specified test data. 
+--
+-- If test data should be generated by the system, users must specify the size 
+-- of the data to be generated. This is achieved using @Gen l s u@, 
+-- which specifies as a size /range/ by a lower bound @l@, an upper bound 
+-- @u@, and a step @s@. This is converted to a Haskell range 
+-- @[l, (l + s) .. u]@ and a test input is generated for each size in this
+-- list. 
+-- For example: @Gen 5 5 100@ corresponds to the range @[5, 10, 15 .. 100]@. 
 --
 -- 'TestSuite's require a minimum number of /distinctly sized/ inputs: see 
 -- 'minInputs'.
 --
--- __Important__: Each test datatype must incorporate a /sensible/ notion of 
--- size, which must be codified in its corresponding 'Arbitrary' instance in
--- order to use the @Gen@ option correctly. For example, in the case of lists a 
--- natural way of encoding size is number of elements. 
 -- __**Incorrectly sized test data will lead to erroneous performance results**__.
 data DataOpts = 
-    Manual String       -- ^ The system should search for manually specified test data
-                        --   with the corrresponding identifier in the user input file.
+    Manual Id           -- ^ The system should search for user-specified test data
+                        --   with the given name in the user input file.
   | Gen Int Int Int     -- ^ The system should generate random test data in the given size range.
- -- | Discover          -- ^ <TO-DO>: The system should discover compatible 'Manual' data, or 
-                        -- accept a suitable 'Gen' setting at a later time. 
-    deriving Show
+ -- | Discover          -- ^ <TO-DO>: The system should discover compatible user-specified 
+                        -- data, or accept a suitable 'Gen' setting at a later time.
+    deriving Eq
 
 instance Default DataOpts where 
   def = Gen 5 5 100
+
+-- | Each test suite requires a minimum number of distinctly sized test inputs.
+-- 
+-- > minInputs = 20
+minInputs :: Int 
+minInputs  = 20
 
 
 
@@ -226,4 +261,30 @@ instance Default AnalOpts where
 
 
 
+-- * Errors 
 
+-- ** Input errors
+
+-- | Errors relating to user inputs, whether it be erroneous definitions 
+-- in user input files or filepath/file access errors. In general, the system 
+-- will attempt to continue with its execution for as long as possible. 
+-- Therefore, unless a critical error is encountered, such as a filepath or 
+-- file access error, the system will collate all user input errors, and then
+-- summarise them after the user input file has been fully analysed.
+data InputError = 
+    FilePathErr String   -- ^ Invalid filepath.
+  | FileErr     String   -- ^ File access error.
+  | TestOptsErr String   -- ^ Invalid test options.
+  | DataOptsErr String   -- ^ Invalid data options.
+  | AnalOptsErr String   -- ^ Invalid statistical analysis options.
+  | InstanceErr String   -- ^ One or more missing instance declarations.
+
+instance Show InputError where 
+  show (FilePathErr s) = "File path error: "        ++ s
+  show (FileErr     s) = "File error: "             ++ s
+  show (TestOptsErr s) = "Test options error: "     ++ s
+  show (DataOptsErr s) = "Test data error: "        ++ s
+  show (AnalOptsErr s) = "Analysis options error: " ++ s
+  show (InstanceErr s) = "Instance error: "         ++ s
+
+instance Exception InputError
