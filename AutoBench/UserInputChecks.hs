5420ec61ecac0228980085db1be5ada77b237845
Martin Handley <psxmah@nottingham.ac.uk>
2018-05-18 07:29:35 +0100

    latest

---
+++ AutoBench/UserInputChecks.hs
@@ -0,0 +1,24 @@
+
+{-# OPTIONS_GHC -Wall #-}
+
+{-|
+
+  Module      : AutoBench.UserInputChecks
+  Description : Interpreting, validating and classifying user inputs.
+  Copyright   : (c) 2018 Martin Handley
+  License     : BSD-style
+  Maintainer  : martin.handley@nottingham.ac.uk
+  Stability   : Experimental
+  Portability : GHC
+
+  
+
+  
+
+-}
+
+module AutoBench.UserInputChecks
+  (
+
+  ) where 
+
