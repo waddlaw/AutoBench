5aa83a360af04044319beef6dfecac9ac0ddfde1
Martin Handley <psxmah@nottingham.ac.uk>
2018-05-25 08:35:41 +0100

    latest

--- AutoBench/Internal/Hint.hs
+++ AutoBench/Internal/Hint.hs
@@ -3,10 +3,9 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiWayIf #-}
 
-
 {-|
 
-  Module      : AutoBench.Hint
+  Module      : AutoBench.Internal.Hint
   Description : Dynamically interpreting user input files.
   Copyright   : (c) 2018 Martin Handley
   License     : BSD-style
@@ -14,7 +13,12 @@
   Stability   : Experimental
   Portability : GHC
 
-  This module is responsible for dynamically interpreting user input files.
+  This module is responsible for dynamically interpreting user input files, 
+  for example:
+
+  * Loading files and setting top level modules;
+  * Loading AutoBench helper modules used for dynamic checking;
+  * Extracting definitions and typing information for user input files.
 
 -}
 
@@ -25,10 +29,10 @@
    - 
 -}
 
-module AutoBench.Hint
+module AutoBench.Internal.Hint
   (
     loadFileSetTopLevelModule               -- Load a file and set the top level module.
-  , loadFileSetTopLevelModuleWithHelpers    -- Load a file and set the top level module; also load helper modules.
+  , loadFileSetTopLevelModuleWithHelpers    -- Load a file and set the top level module; also load AutoBench helper modules.
   , extractElemsAndTypes                    -- From a previously loaded file, extract all definitions and their corresponding types if appropriate.
 
   ) where
@@ -49,36 +53,44 @@ import AutoBench.Types          (InputError(..), SystemError(..))
 import AutoBench.Utils          (filepathToModuleName)
 
 -- | Load a file and set the top level module. The module name is calculated
--- from the file name by simply dropping the \'.hs\' extension. For example, if
+-- from the filename by simply dropping the \'.hs\' extension. For example, if
 -- the given file is named \'Input.hs\', then module name is assumed to be
 -- \'Input\'.
 -- 
--- If the file doesn't have a module name that matches the file name, an
--- 'InputError' is thrown.
+-- If the file doesn't have a module name that matches the filename, an
+-- 'InputError' is thrown. 
+--
+-- N.B. the system requires that module names match filenames.
 loadFileSetTopLevelModule :: MonadInterpreter m => FilePath -> m ()
 loadFileSetTopLevelModule fp = do
-  let mn = filepathToModuleName fp
+  let mn = filepathToModuleName fp  -- Drop the .hs extension.
   loadModules [fp]
-  mods <- getLoadedModules
+  mods <- getLoadedModules          -- Make sure module is successfully loaded.
   if mn `elem` mods
   then setTopLevelModules [mn]        
   else throwM (FileErr "Invalid module name.")
 
 -- | Similar to 'loadFileSetTopLevelModule' but loads additional helper 
--- modules. In practice, this is used for user input checking, 
+-- modules. In practice, this is used for dynamically checking user input files:
 -- see AutoBench.UserInputChecks.
+--
+-- For AutoBench purposes:
+-- 'hint' requires that all modules are loaded at the same, so the system cannot 
+-- simply load additional modules when they are required (e.g., for the dynamic
+-- checking phase). Instead, all modules have to be (re-)loaded, including the 
+-- user input file.
 loadFileSetTopLevelModuleWithHelpers 
   :: MonadInterpreter m 
   => FilePath 
-  -> [ModuleName] 
+  -> [ModuleName]  -- Helper modules.
   -> m ()
 loadFileSetTopLevelModuleWithHelpers fp helpers = do
   let mn = filepathToModuleName fp
   loadModules (fp : helpers)
   mods <- getLoadedModules
-  if | mn `notElem` mods -> 
-         throwM (FileErr "Invalid module name.")
-     | length (mods `intersect` helpers) /= length helpers -> 
+  if | mn `notElem` mods ->                                             -- Firstly check the user input file is loaded.
+         throwM (FileErr "Invalid module name.")                       
+     | length (mods `intersect` helpers) /= length helpers ->           -- Then check all the AutoBench helper modules are loaded.
          throwM (InternalErr $ "loadFileSetTopLevelModuleWithHelpers: failed to load one or more helper modules: " ++ show helpers)
      | otherwise -> setTopLevelModules (mn : helpers)        
 
@@ -88,9 +100,9 @@ extractElemsAndTypes
   :: MonadInterpreter m 
   => ModuleName 
   -> m [(ModuleElem, Maybe TypeString)] 
-extractElemsAndTypes mn = do
-  defs <- getModuleExports mn
-  tys  <- mapM (\case
+extractElemsAndTypes mn = do     
+  defs <- getModuleExports mn     -- Get the module definitions.
+  tys  <- mapM (\case             -- Can only get typing information from 'Fun's.
     Nothing  -> return Nothing 
     Just idt -> Just <$> typeOf idt) (fmap funIdt defs)
   return (zip defs tys)
