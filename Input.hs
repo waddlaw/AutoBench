7a0647778eee7fda73b08658b02ffd2ab7f3f472
Martin Handley <psxmah@nottingham.ac.uk>
2018-06-02 19:15:25 +0100

    latest

--- Input.hs
+++ Input.hs
@@ -7,13 +7,6 @@ import AutoBench.Types
 import Data.Default
 
 
-import AutoBench.QuickBench
-
-import Prelude hiding (id, const)
-
-
-import qualified Data.List as List
-
 import Test.QuickCheck (Arbitrary, arbitrary, generate, resize, sized, vectorOf)
 
 
@@ -24,7 +17,6 @@ instance {-# OVERLAPPING #-} Arbitrary [Int] where
   arbitrary = sized $ \n -> vectorOf n arbitrary
 
 
-{-
 slowRev :: [Int] -> [Int]
 slowRev []       = []
 slowRev (x : xs) = slowRev xs ++ [x]
@@ -33,25 +25,7 @@ fastRev :: [Int] -> [Int]
 fastRev xs = go xs []
   where 
     go [] ys       = ys 
-    go (x : xs) ys = go xs (x : ys) -}
-
-
+    go (x : xs) ys = go xs (x : ys) 
 
 ts :: TestSuite 
-ts  = def { _dataOpts = Gen 5 5 100, _progs = ["sort"], _ghcFlags = ["loll", "lolll", "llllol", "lllloll", "lolll", "lollll", "lopppl", "oolol", "loiiil", "loooiol", "loyyhl"] }
-
-
-const :: Int -> Int -> Int
-const x y = x 
-
-const2 :: Int -> Int -> Int
-const2 x y = x 
-
-sort :: [Int] -> [Int] 
-sort  = List.sort
-
-sort2 :: [Int] -> [Int]
-sort2  = sort
-
-sort3333 = Input.sort
-sort4 = Input.sort
+ts  = def 
