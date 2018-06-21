ca06761636153f5f85b64b4eef345e88752c3fb8
Martin Handley <psxmah@nottingham.ac.uk>
2018-06-21 08:55:11 +0100

    Code review.

--- src/AutoBench/Internal/DataGeneration.hs
+++ src/AutoBench/Internal/DataGeneration.hs
@@ -1,30 +1,26 @@
 
 {-# OPTIONS_GHC -Wall #-}
 
-{-|
+-- |
+--
+-- Module      : AutoBench.Internal.DataGeneration
+-- Description : Generate test data using QuickCheck
+-- Copyright   : (c) 2018 Martin Handley
+-- License     : BSD-style
+-- Maintainer  : martin.handley@nottingham.ac.uk
+-- Stability   : Experimental
+-- Portability : GHC
+--
+-- This module is responsible for generating test data using QuickCheck.
+-- 
+-- The types do all the work here, so when these functions are used in 
+-- a specific context, they generate the right data. Only the size
+-- range of the inputs to be generated must be specified using 'DataOpts'.
+--
 
-  Module      : AutoBench.Internal.DataGeneration
-  Description : Generate test data using QuickCheck.
-  Copyright   : (c) 2018 Martin Handley
-  License     : BSD-style
-  Maintainer  : martin.handley@nottingham.ac.uk
-  Stability   : Experimental
-  Portability : GHC
-
-  This module is responsible for generating test data using QuickCheck.
-  
-  The types do all the work here, so when these functions are used in 
-  a specific context, they generate the right data. Only the size
-  range of the inputs to be generated must be specified using 'DataOpts'.
-
--}
-
-{-
-   ----------------------------------------------------------------------------
-   <TO-DO>:
-   ----------------------------------------------------------------------------
-   - 
--}
+-------------------------------------------------------------------------------
+-- <TO-DO>:
+-------------------------------------------------------------------------------
 
 module AutoBench.Internal.DataGeneration 
   (
@@ -38,8 +34,8 @@ import Test.QuickCheck (Arbitrary, arbitrary, generate, resize)
 import AutoBench.Internal.Types (DataOpts(..), toHRange)
 
 -- | Generate test data for binary test programs in the given size range.
--- Using the size range 'Gen l s u' and generating all pairs.
--- I.e., [5,10..100] => [(5,5),(5, 10)..(5,100),(10, 5)..(100, 100)] 
+-- Generate all pairs in the size range 'Gen l s u':
+-- @[5,10..100] ===> [(5,5),(5, 10)..(5,100),(10, 5)..(100, 100)]@ 
 genDataBin :: (Arbitrary a, Arbitrary b) => DataOpts -> [IO (a, b)] 
 genDataBin dOpts = 
   [ (,) <$> generate (resize s1 arbitrary)   
